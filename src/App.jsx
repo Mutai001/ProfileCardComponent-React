6fc28e5bc0a2c41cb56cc5f961bb4a56de28a184
Mutai001 <omusekim@gmail.com>
2024-06-18 15:05:12 +0300

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ProfileCard from './components/ProfileCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-.
-
+    <div className="app">
+      <ProfileCard 
+        name="Cyrus Kimutai"
+        age={23}
+        profilePicture="https://avatars.githubusercontent.com/u/77490521?v=4"
+        bio="I am a software developer"
+      />
+      <ProfileCard 
+        name="Cyrus Kimutai"
+        age={23}
+        profilePicture="https://avatars.githubusercontent.com/u/77490521?v=4"
+        bio="I am a software developer"
+      />
+      <ProfileCard 
+        name="Cyrus Kimutai"
+        age={23}
+        profilePicture="https://avatars.githubusercontent.com/u/77490521?v=4"
+        bio="I am a software developer"
+      />
+    </div>
     </>
   )
 }
